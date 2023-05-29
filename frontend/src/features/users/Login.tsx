330b17d2508898bf8126fab8e5bc1113438766ea
Edil Biktimirov <idyabik@gmail.com>
2023-05-29 10:42:42 +0600

    Added Login.tsx, Register.tsx, usersSlice.ts, usersThunks.ts

---
+++ frontend/src/features/users/Login.tsx
@@ -0,0 +1,116 @@
+import React, {useState} from 'react';
+import {useAppDispatch, useAppSelector} from '../../app/hooks';
+import {selectLoginError, selectLoginLoading} from './usersSlice';
+import {googleLogin, login} from './usersThunks';
+import {Alert, Avatar, Box, Container, Grid, Link, TextField, Typography} from '@mui/material';
+import {LoadingButton} from "@mui/lab";
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+import {Link as RouterLink, useNavigate} from 'react-router-dom';
+import {GoogleLogin} from "@react-oauth/google";
+import type {LoginMutation} from '../../types';
+
+const Login = () => {
+  const dispatch = useAppDispatch();
+  const error = useAppSelector(selectLoginError);
+  const loading = useAppSelector(selectLoginLoading);
+  const navigate = useNavigate();
+
+  const [state, setState] = useState<LoginMutation>({
+    email: '',
+    password: '',
+  });
+
+  const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = event.target;
+    setState(prevState => ({...prevState, [name]: value}));
+  };
+
+  const submitFormHandler = async (event: React.FormEvent) => {
+    event.preventDefault();
+    await dispatch(login(state)).unwrap();
+    navigate('/');
+  };
+
+  const googleLoginHandler = async (credential: string) => {
+    await dispatch(googleLogin(credential)).unwrap();
+    navigate('/');
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        style={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+          <LockOpenIcon/>
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box sx={{pt: 2}}>
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              if (credentialResponse.credential) {
+                void googleLoginHandler(credentialResponse.credential);
+              }
+            }}
+            onError={() => {
+              console.log('Login Failed');
+            }}
+          />
+        </Box>
+        {error && (
+          <Alert severity="error" sx={{mt: 3, width: '100%'}}>
+            {error.error}
+          </Alert>
+        )}
+        < Box component="form" onSubmit={submitFormHandler} sx={{mt: 3}}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                label="Email"
+                name="email"
+                autoComplete="current-email"
+                value={state.email}
+                onChange={inputChangeHandler}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                value={state.password}
+                onChange={inputChangeHandler}
+              />
+            </Grid>
+          </Grid>
+          <LoadingButton
+            type="submit"
+            loading={loading}
+            fullWidth
+            variant="contained"
+            sx={{mt: 3, mb: 2}}
+          >
+            Sign In
+          </LoadingButton>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link component={RouterLink} to="/register" variant="body2">
+                Or sign up
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Login;
