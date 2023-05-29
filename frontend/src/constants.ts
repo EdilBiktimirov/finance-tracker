db418ba5e0a0e3f46ebea4d6e28e337bcc4a8fcd
Edil Biktimirov <idyabik@gmail.com>
2023-05-29 10:34:39 +0600

    Added constants.ts

---
+++ frontend/src/constants.ts
@@ -0,0 +1,3 @@
+export const apiUrl = 'http://localhost:8000';
+export const GOOGLE_CLIENT_ID = '297119171949-73cvdtpi0ongger6v5067oafv90ogscd.apps.googleusercontent.com';
+export const TRANSACTION_CATEGORY = ['income', 'expenses'];
