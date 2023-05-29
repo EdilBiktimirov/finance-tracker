9e9461005bdaf08a27c68bcc80b10e35717b931d
Edil Biktimirov <idyabik@gmail.com>
2023-05-29 10:34:14 +0600

    Added axiosApi.ts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import Layout from "./components/UI/Layout";
+import Register from "./features/users/Register";
+import Login from "./features/users/Login";
+import Accounts from "./features/accounts/Accounts";
+import AccountTypesForm from "./features/accountTypes/components/accountTypesForm";
+import AddAccount from "./features/accounts/components/AddAccount";
+import Transactions from "./features/transactions/Transactions";
+import CategoriesForm from "./features/categories/components/CategoriesForm";
+import AddTransaction from "./features/transactions/AddTransaction";
+import Statistics from "./features/statistics/Statistics";
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <Layout>
+        <Routes>
+          <Route path={'/'} element={<Accounts/>}/>
+          <Route path={'/accounts/:id'} element={<Transactions/>}/>
+          <Route path={'/add-new-account-type'} element={<AccountTypesForm/>}/>
+          <Route path={'/add-new-account'} element={<AddAccount/>}/>
+          <Route path={'/add-new-category'} element={<CategoriesForm/>}/>
+          <Route path={'/add-new-transaction'} element={<AddTransaction/>}/>
+          <Route path={'/statistics/:id'} element={<Statistics/>}/>
+          <Route path={'/register'} element={<Register/>}/>
+          <Route path={'/login'} element={<Login/>}/>
+          <Route path={'*'} element={<h2>Not found</h2>}/>
+        </Routes>
+      </Layout>
+    </>
   );
 }
 
