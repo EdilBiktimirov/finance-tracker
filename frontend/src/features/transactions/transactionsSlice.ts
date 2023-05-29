9e5704b50ade26c9538249244a1a66b301e88a06
Edil Biktimirov <idyabik@gmail.com>
2023-05-29 11:01:35 +0600

    Added transactionsSlice.ts, transactionsThunks.ts

---
+++ frontend/src/features/transactions/transactionsSlice.ts
@@ -0,0 +1,102 @@
+import {Statistics, Transaction, ValidationError} from "../../types";
+import {createSlice} from "@reduxjs/toolkit";
+import {RootState} from "../../app/store";
+import {createTransaction, fetchTransactions, removeTransaction, searchTransactions} from "./transactionsThunks";
+
+interface TransactionsState {
+  transactions: Transaction[];
+  statistics: Statistics[];
+  loading: boolean;
+  loadingSearch: boolean,
+  loadingCreateTransaction: boolean;
+  loadingRemoveTransaction: false | string;
+  error: boolean;
+  createTransactionError: ValidationError | null;
+}
+
+const initialState: TransactionsState = {
+  transactions: [],
+  statistics: [],
+  loading: false,
+  loadingSearch: false,
+  loadingCreateTransaction: false,
+  loadingRemoveTransaction: false,
+  error: false,
+  createTransactionError: null,
+};
+
+export const transactionsSlice = createSlice({
+  name: 'transactions',
+  initialState,
+  reducers: {
+    cleanStatistics: (state) => {
+      state.statistics = [];
+    }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchTransactions.pending, (state) => {
+      state.loading = true;
+      state.error = false;
+    });
+    builder.addCase(fetchTransactions.fulfilled, (state, action) => {
+      state.loading = false;
+      state.transactions = action.payload;
+      state.error = false;
+    });
+    builder.addCase(fetchTransactions.rejected, (state) => {
+      state.loading = false;
+      state.error = true;
+    });
+
+    builder.addCase(searchTransactions.pending, (state) => {
+      state.loadingSearch = true;
+      state.error = false;
+    });
+    builder.addCase(searchTransactions.fulfilled, (state, action) => {
+      state.loadingSearch = false;
+      state.statistics = action.payload;
+      state.error = false;
+    });
+    builder.addCase(searchTransactions.rejected, (state) => {
+      state.loadingSearch = false;
+      state.error = true;
+    });
+
+
+    builder.addCase(createTransaction.pending, (state) => {
+      state.loadingCreateTransaction = true;
+      state.createTransactionError = null;
+    });
+    builder.addCase(createTransaction.fulfilled, (state) => {
+      state.loadingCreateTransaction = false;
+    });
+    builder.addCase(createTransaction.rejected, (state, {payload: error}) => {
+      state.loadingCreateTransaction = false;
+      state.createTransactionError = error || null;
+    });
+    builder.addCase(removeTransaction.pending, (state, {meta}) => {
+      state.loadingRemoveTransaction = meta.arg;
+      state.error = false;
+    });
+    builder.addCase(removeTransaction.fulfilled, (state) => {
+      state.loadingRemoveTransaction = false;
+      state.error = false;
+    });
+    builder.addCase(removeTransaction.rejected, (state) => {
+      state.loadingRemoveTransaction = false;
+      state.error = true;
+    });
+  }
+});
+
+export const transactionsReducer = transactionsSlice.reducer;
+
+export const {cleanStatistics} = transactionsSlice.actions;
+
+export const selectTransactions = (state: RootState) => state.transactions.transactions;
+export const selectStatistics = (state: RootState) => state.transactions.statistics;
+export const selectLoading = (state: RootState) => state.transactions.loading;
+export const selectSearchLoading = (state: RootState) => state.transactions.loadingSearch;
+export const selectLoadingCreateTransaction = (state: RootState) => state.transactions.loadingCreateTransaction;
+export const selectLoadingRemoveTransaction = (state: RootState) => state.transactions.loadingRemoveTransaction;
+export const selectCreateTransactionError = (state: RootState) => state.transactions.createTransactionError;
---
+++ frontend/src/features/transactions/transactionsThunks.ts
@@ -0,0 +1,61 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import {Statistics, StatisticsMutation, Transaction, TransactionMutation, ValidationError} from "../../types";
+import axiosApi from "../../axiosApi";
+import {RootState} from "../../app/store";
+import {isAxiosError} from "axios";
+import {enqueueSnackbar} from "notistack";
+
+export const fetchTransactions = createAsyncThunk<Transaction[], string | undefined>(
+  'transactions/fetchAll',
+  async (id) => {
+    if (id) {
+      const response = await axiosApi.get<Transaction[]>('/transactions/' + id);
+      return response.data;
+    } else {
+      const response = await axiosApi.get<Transaction[]>('/transactions');
+      return response.data;
+    }
+  });
+
+export const createTransaction = createAsyncThunk<void, TransactionMutation, { state: RootState, rejectValue: ValidationError }>(
+  'transactions/createTransaction',
+  async (transaction, {getState, rejectWithValue}) => {
+    try {
+      const user = getState().users.user;
+
+      if (user) {
+        const transactionWithCorrectDate = {
+          ...transaction,
+          createdAt: transaction.createdAt?.format('YYYY-MM-DD HH:mm')
+        }
+        await axiosApi.post('/transactions', transactionWithCorrectDate);
+      }
+    } catch (e) {
+      if (isAxiosError(e) && e.response && e.response.status === 400) {
+        return rejectWithValue(e.response.data as ValidationError);
+      }
+      if (isAxiosError(e) && e.response && e.response.status === 403) {
+        enqueueSnackbar(e.response.data.message, {variant: 'error'});
+      }
+      throw e;
+    }
+  });
+
+export const removeTransaction = createAsyncThunk<void, string>(
+  'transactions/removeOne',
+  async (id) => {
+    try {
+      await axiosApi.delete('/transactions/' + id);
+    } catch {
+      throw new Error();
+    }
+  });
+
+export const searchTransactions = createAsyncThunk<Statistics[], StatisticsMutation>(
+  'transactions/search',
+  async (data) => {
+    const response = await axiosApi.get<Statistics[]>(
+      '/transactions/search/?start=' + data.start?.format('YYYY-MM-DD') + '&end='
+      + data.end?.format('YYYY-MM-DD') + '&category=' + data.category + '&account=' + data.account);
+    return response.data;
+  });
