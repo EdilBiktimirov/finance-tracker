25fcdeaab6335a681ad5e7ef09600e613a2a885f
Edil Biktimirov <idyabik@gmail.com>
2023-05-29 10:35:21 +0600

    Added theme.ts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {persister, store} from "./app/store";
+import {PersistGate} from "redux-persist/integration/react";
+import {ThemeProvider} from "@mui/material";
+import {addInterceptors} from "./axiosApi";
+import theme from "./theme";
 import App from './App';
+import {GoogleOAuthProvider} from "@react-oauth/google";
+import {GOOGLE_CLIENT_ID} from "./constants";
+import './index.css';
+
+addInterceptors(store);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <Provider store={store}>
+      <PersistGate persistor={persister}>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <App/>
+          </ThemeProvider>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </GoogleOAuthProvider>
 );
